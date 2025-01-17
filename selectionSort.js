86c8a19da28a768cb573e52564ec283ef8fea381
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-17 04:21:24 -0300

    feat: sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,4 +1,13 @@
 // sort is a function that takes an array of numbers and returns a sorted array.
 const books = require('./bookList');
 const lowerPrice = require('./lowerPrice');
-for (let current = 0; current < books.length; current++) {}
+for (let current = 0; current < books.length; current++) {
+	let cheaper = lowerPrice(books, current);
+	let aux = books[current];
+	let cheaperBook = books[cheaper];
+
+	books[current] = cheaperBook;
+	books[cheaper] = aux;
+}
+
+console.log(books);
