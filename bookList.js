21bef699be103d6bc6e57487c94c562672acf379
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-17 02:21:35 -0300

    feat: booklist

---
+++ bookList.js
@@ -0,0 +1,42 @@
+const booksPrice = [
+	{
+		title: 'El principito',
+		price: 100,
+	},
+	{
+		title: 'El señor de los anillos',
+		price: 200,
+	},
+	{
+		title: 'Harry Potter',
+		price: 300,
+	},
+	{
+		title: 'Cien años de soledad',
+		price: 400,
+	},
+	{
+		title: 'O poder da ação',
+		price: 500,
+	},
+	{
+		title: 'O poder do hábito',
+		price: 430,
+	},
+	{
+		title: 'O poder do agora',
+		price: 230,
+	},
+	{
+		title: 'O poder do subconsciente',
+		price: 170,
+	},
+	{
+		title: 'O poder do mito',
+		price: 192,
+	},
+	{
+		title: 'O poder do pensamento positivo',
+		price: 1000,
+	},
+];
